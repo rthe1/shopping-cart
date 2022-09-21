6366b85fab79f8599f5a62872f0465ea2aa850f8
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-09-21 06:13:01 -0400

    context provider functionscomplete

--- src/context/ShoppingCartContext.ts
+++ src/context/ShoppingCartContext.ts
@@ -1,4 +1,4 @@
-import {useContext, createContext, ReactNode} from 'react';
+import {useState, useContext, createContext, ReactNode} from 'react';
 
 type ShoppingCartProviderProps = {
   children: ReactNode
@@ -11,18 +11,58 @@ type ShoppingCartContext = {
   removeFromCart: (id: number) => void
 }
 
+ 
+
 const ShoppingCartContext = createContext({} as ShoppingCartContext);
 
+const [cartItems,setCartItems] = useState()
+
+const getItemQuantity = (id) => cartItems.find(item => item.id === id)?.quantity || 0;
+
+const IncreaseCartQuantity = (id) => {
+  setCartItems(currItems => {
+
+   if(currItems.find(item => item.id === id) == null){
+    return([...currItems, id: id, quantity: 1])
+  } else {
+    return([...currItems, item: id, quantity: quantity + 1])
+  }
+ }
+)
+}   
+
+const DecreaseCartQuantity = (id) => {
+  setCartItems(currItems => {
+
+   if(currItems.find(item => item.id === id)?.quantity == 1){
+    return(currItems.filter(item => item.id !== id))
+  } else {
+    return([...currItems, item: id, quantity: quantity - 1])
+  }
+ }
+)
+}
+
+const removeFromCart = (id) => {
+  setCartItems(currItems => return(currItems.filter(item => item.id !== id)))
+}
+
+
+
 export function useShoppingCart () {
-  return (
-    useContext(ShoppingCartContext)
-  )
+  return useContext(ShoppingCartContext)
 }
 
 export function ShoppingCartContextProvider ({children}: ShoppingCartProviderProps) {
   return (
-    <ShoppingCartContext.Provider value={{}}>
+    <ShoppingCartContext.Provider value={{
+      removeFromCart, 
+      DecreaseCartQuantity, 
+      getItemQuantity
+      IncreaseCartQuantity }}>
+
     {children}
+    
     </ShoppingCartContext.Provider>
   )
 }
